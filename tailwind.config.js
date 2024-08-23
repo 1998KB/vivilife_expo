382fa19341a0d9dfb522ca03ecf7fa77aaf62471
Yaoyu Chen <43839115+yych42@users.noreply.github.com>
2024-08-23 17:26:47 +0200

    setup font files

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {     
+      colors: {
         customGreen: "rgba(9, 69, 5, .5)",
         customGreenTab: "rgba(9, 69, 5, .3)",
         darkGreen: "#059212",
@@ -18,6 +18,9 @@ module.exports = {
         lightGreen: "#9BEC00",
         darkerGreen: "#094505",
         greenLowKey: "#157A6E",
+      },
+      fontFamily: {
+        sans: ["Apfel", "Helvetica", "sans-serif"],
       },
     },
   },
